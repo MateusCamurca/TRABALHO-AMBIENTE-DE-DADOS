c9d732eec26c5561bb8ffd620a1f0ea7bd0daf2b
cleanyteixeira <cleanyteixeira02@gmail.com>
2023-11-29 22:46:02 -0300

    contribuicao do metado

--- CalculadoraMultiplicacao.java
+++ CalculadoraMultiplicacao.java
@@ -7,4 +7,12 @@ public class Calculadoramultiplicacao {
 public double multiplicar (double a, double b){
     return a * b;
 }
+public static void main(String[] args) {
+        CalculadoraMultiplicacao calculadora = new CalculadoraMultiplicacao();
+        int resultadoMultiplicacaoInt = calculadora.multiplicar(5, 3);
+        double resultadoMultiplicacaoDouble = calculadora.multiplicar(2.5, 4.2);
+        System.out.println("Resultado da multiplicação (int): " + resultadoMultiplicacaoInt);
+        System.out.println("Resultado da multiplicação (double): " + resultadoMultiplicacaoDouble);
+    }
+
 }
